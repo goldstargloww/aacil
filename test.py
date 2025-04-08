cd238e6bfcf8f9bb90927f61f793be3eaddf1d12
goldstargloww <goldstargloww@gmail.com>
2025-04-07 20:16:31 -0400

    update test file to find duplicate filenames

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import os
-import re
+import os, re, sqlite3
 
 
 def filelist():
@@ -10,35 +9,52 @@ def filelist():
                 print(fname)
 
 
-strings = [
-    '["Fractals, adapted by M"]',
-    '["Petri, adapted by M"]',
-    '["Petri, adapted by M"]',
-    '["Petri, adapted by CC"]',
-    '["Petri, adapted by M"]',
-    '["Petri, adapted by M"]',
-    '["Petri, adapted by M"]',
-    '["Petri, adapted by CC"]',
-    '["Petri, adapted by M"]',
-    '["M, adapted from Petri"]',
-    '["Neon, adapted from froggygolem"]',
-    '["Neon, adapted from froggygolem"]',
-    '["Neon, adapted from froggygolem"]',
-    '["Fractals, adapted by M"]',
-    '["M (adapted from StellarSymbols)"]',
-    '["M, adapted from Plum"]',
-]
+def regex_test():
+    strings = [
+        '["Fractals, adapted by M"]',
+        '["Petri, adapted by M"]',
+        '["Petri, adapted by M"]',
+        '["Petri, adapted by CC"]',
+        '["Petri, adapted by M"]',
+        '["Petri, adapted by M"]',
+        '["Petri, adapted by M"]',
+        '["Petri, adapted by CC"]',
+        '["Petri, adapted by M"]',
+        '["M, adapted from Petri"]',
+        '["Neon, adapted from froggygolem"]',
+        '["Neon, adapted from froggygolem"]',
+        '["Neon, adapted from froggygolem"]',
+        '["Fractals, adapted by M"]',
+        '["M (adapted from StellarSymbols)"]',
+        '["M, adapted from Plum"]',
+    ]
 
-adapted_by_pattern = re.compile(r"\[['\"](.+), adapted by (.+)['\"]\]", re.I)
-adapted_from_pattern = re.compile(r"\[['\"](.+), adapted from (.+)['\"]\]", re.I)
-adapted_from_pattern2 = re.compile(r"\[['\"](.+) \(adapted from (.+)\)['\"]\]", re.I)
+    adapted_by_pattern = re.compile(r"\[['\"](.+), adapted by (.+)['\"]\]", re.I)
+    adapted_from_pattern = re.compile(r"\[['\"](.+), adapted from (.+)['\"]\]", re.I)
+    adapted_from_pattern2 = re.compile(
+        r"\[['\"](.+) \(adapted from (.+)\)['\"]\]", re.I
+    )
 
-for string in strings:
+    for string in strings:
 
-    if re.search(adapted_by_pattern, string):
-        string = re.sub(adapted_by_pattern, r"['\1', '\2']", string)
-    elif re.search(adapted_from_pattern, string):
-        string = re.sub(adapted_from_pattern, r"['\2', '\1']", string)
-    elif re.search(adapted_from_pattern2, string):
-        string = re.sub(adapted_from_pattern2, r"['\2', '\1']", string)
-    print(string)
+        if re.search(adapted_by_pattern, string):
+            string = re.sub(adapted_by_pattern, r"['\1', '\2']", string)
+        elif re.search(adapted_from_pattern, string):
+            string = re.sub(adapted_from_pattern, r"['\2', '\1']", string)
+        elif re.search(adapted_from_pattern2, string):
+            string = re.sub(adapted_from_pattern2, r"['\2', '\1']", string)
+        print(string)
+
+
+conn = sqlite3.connect("symbols.db")
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM symbols")
+symbols = cursor.fetchall()
+
+filenames = []
+for symbol in symbols:
+    file = symbol[0].split("/")[-1]
+    if file in filenames:
+        print(f"duplicate found: {file}")
+    else:
+        filenames.append(file)
