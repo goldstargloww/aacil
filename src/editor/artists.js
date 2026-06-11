9d9b7c28d5e2d6524cecdce9115b8be572d90848
R <r@arcanenibble.com>
2026-06-11 18:16:41 +0100

    Refactor some editor functions into separate files

---
+++ src/editor/artists.js
@@ -0,0 +1,227 @@
+import * as sorting from '../sorting.js';
+
+export async function load_artist_info(database) {
+    let artists_cur_id = document.getElementById('artists_cur_id');
+    let artists_status = document.getElementById('artists_status');
+    let artist_display = document.getElementById('artist_display');
+    let artist_parens = document.getElementById('artist_parens');
+    let artist_footnote = document.getElementById('artist_footnote');
+    let artist_change = document.getElementById('artist_change');
+    let artist_new = document.getElementById('artist_new');
+
+    let artists_merge_victim_id = document.getElementById('artists_merge_victim_id');
+    let artist_merge_details = document.getElementById('artist_merge_details');
+    let artist_merge_button = document.getElementById('artist_merge_button');
+
+    let new_artists_select;
+    let new_artists_merge_select;
+
+    function reset_ui() {
+        // Load all the existing artists
+        let all_artists = [];
+        database.exec(`select * from artists`, {
+            rowMode: 'object',
+            resultRows: all_artists,
+        });
+        all_artists.sort(sorting.sort_artists);
+
+        // Reset all the relevant UI
+        artists_status.innerHTML = '&nbsp;';
+        artists_cur_id.innerHTML = '&nbsp;';
+        artist_display.value = '';
+        artist_parens.value = '';
+        artist_footnote.value = '';
+        artist_display.focus();
+        // Temporarily remove this, so that we can programmatically change the default button
+        artist_change.remove();
+        artist_change.disabled = true;
+        artist_change.style.display = 'none';
+        artist_merge_details.style.display = 'none';
+
+        // Make entirely new list of artists
+        new_artists_select = document.createElement('select');
+        new_artists_select.id = 'artists_select'
+        // Add an empty option
+        new_artists_select.appendChild(document.createElement('option'));
+
+        new_artists_merge_select = document.createElement('select');
+        new_artists_merge_select.id = 'artists_merge_select'
+
+        let all_artists_map = new Map();
+        for (let artist of all_artists) {
+            let option = document.createElement('option');
+            option.value = artist.id;
+            option.innerText = artist.display;
+            new_artists_select.appendChild(option);
+
+            option = document.createElement('option');
+            option.value = artist.id;
+            option.innerText = artist.display;
+            new_artists_merge_select.appendChild(option);
+
+            all_artists_map.set(artist.id, artist);
+        }
+
+        new_artists_select.addEventListener('change', () => {
+            let selected_artist_id = new_artists_select.value;
+            if (selected_artist_id) {
+                artists_cur_id.innerText = `Selected artist ID: ${selected_artist_id}`;
+                selected_artist_id = BigInt(selected_artist_id);
+
+                let artist = all_artists_map.get(selected_artist_id);
+                artist_display.value = artist.display;
+                artist_parens.value = artist.front_page_parens;
+                artist_footnote.value = artist.front_page_footnote;
+
+                artist_new.parentNode.insertBefore(artist_change, artist_new);
+                artist_change.disabled = false;
+                artist_change.style.display = '';
+                artist_merge_details.style.display = '';
+            } else {
+                artists_cur_id.innerHTML = '&nbsp;';
+                artist_display.value = '';
+                artist_parens.value = '';
+                artist_footnote.value = '';
+
+                artist_change.remove();
+                artist_change.disabled = true;
+                artist_change.style.display = 'none';
+                artist_merge_details.style.display = 'none';
+            }
+        });
+
+        new_artists_merge_select.addEventListener('change', () => {
+            let selected_artist_id = new_artists_merge_select.value;
+            if (selected_artist_id) {
+                artists_merge_victim_id.innerText = `Selected artist ID: ${selected_artist_id}`;
+            } else {
+                artists_merge_victim_id.innerHTML = '&nbsp;';
+            }
+        });
+
+        let old_artists_select = document.getElementById('artists_select');
+        old_artists_select.parentNode.replaceChild(new_artists_select, old_artists_select);
+
+        let old_artists_merge_select = document.getElementById('artists_merge_select');
+        old_artists_merge_select.parentNode.replaceChild(new_artists_merge_select, old_artists_merge_select);
+    }
+
+    // The buttons to actually do things
+    function perform_input_validation(read_id) {
+        let new_display = artist_display.value;
+        let new_parens = artist_parens.value;
+        let new_footnote = artist_footnote.value;
+
+        if (!new_display) {
+            artist_display.focus();
+            artists_status.className = "status_error";
+            artists_status.innerText = "Must have a name";
+            return;
+        }
+
+        if (!new_parens) {
+            new_parens = null;
+        }
+        if (!new_footnote) {
+            new_footnote = null;
+        }
+
+        let query_data = [new_display, new_footnote, new_parens];
+        if (read_id) {
+            query_data.push(BigInt(new_artists_select.value));
+        }
+        return {
+            query_insert_args: "(display, front_page_footnote, front_page_parens, id)",
+            query_update_args: "display = ?, front_page_footnote = ?, front_page_parens = ?",
+            query_data,
+        };
+    }
+
+    artist_change.onclick = () => {
+        let changed_artist = perform_input_validation(true);
+        if (changed_artist === undefined) return;
+
+        let { query_update_args, query_data } = changed_artist;
+        database.exec(`update artists set ${query_update_args} where id = ?`, {
+            bind: query_data
+        });
+
+        // Ok
+        reset_ui();
+
+        artists_status.className = "status_ok";
+        artists_status.innerText = "OK!";
+        download_changes_elem.style.visibility = '';
+    };
+    artist_new.onclick = () => {
+        let new_artist = perform_input_validation(false);
+        if (new_artist === undefined) return;
+
+        let { query_insert_args, query_data } = new_artist;
+
+        let new_id = Snowflake.generate();
+        query_data.push(new_id);
+
+        let query_values = []
+        for (let i = 0; i < query_data.length; i++)
+            query_values.push('?');
+        query_values = query_values.join(', ');
+
+        database.exec(`insert into artists ${query_insert_args} values (${query_values})`, {
+            bind: query_data
+        });
+
+        // Ok
+        reset_ui();
+
+        artists_status.className = "status_ok";
+        artists_status.innerText = `OK, new id ${new_id}!`;
+        download_changes_elem.style.visibility = '';
+    };
+
+    artist_merge_button.onclick = () => {
+        let artist_merge_into = new_artists_select.value;
+        if (!artist_merge_into) {
+            artists_status.className = "status_error";
+            artists_status.innerText = "No artist selected to merge into";
+            return;
+        }
+        artist_merge_into = BigInt(artist_merge_into);
+
+        let artist_merge_victim = artists_merge_select.value;
+        if (!artist_merge_victim) {
+            artists_status.className = "status_error";
+            artists_status.innerText = "No artist selected to merge into";
+            return;
+        }
+        artist_merge_victim = BigInt(artist_merge_victim);
+
+        if (artist_merge_into == artist_merge_victim) {
+            artists_status.className = "status_error";
+            artists_status.innerText = "Selected the same artists";
+            return;
+        }
+
+        database.transaction((txn) => {
+            txn.exec(`update sym_artists set artist_id = ? where artist_id = ?`, {
+                bind: [artist_merge_into, artist_merge_victim],
+            });
+            txn.exec(`update sym_derived_from set artist_id = ? where artist_id = ?`, {
+                bind: [artist_merge_into, artist_merge_victim],
+            });
+            txn.exec(`delete from artists where id = ?`, {
+                bind: [artist_merge_victim]
+            });
+        });
+
+        // Ok
+        reset_ui();
+
+        artists_status.className = "status_ok";
+        artists_status.innerText = "OK!";
+        download_changes_elem.style.visibility = '';
+    }
+
+    // Set up the UI the first time
+    reset_ui();
+}
---
+++ src/editor/symbol_cws.js
@@ -0,0 +1,176 @@
+import * as sorting from '../sorting.js';
+
+export async function load_sym_cw_info(database) {
+    let sym_cw_cur_id = document.getElementById('sym_cw_cur_id');
+    let sym_cw_status = document.getElementById('sym_cw_status');
+    let sym_cw_text = document.getElementById('sym_cw_text');
+    let sym_cw_change = document.getElementById('sym_cw_change');
+    let sym_cw_new = document.getElementById('sym_cw_new');
+    let sym_cw_delete = document.getElementById('sym_cw_delete');
+
+    let all_sym_cw_map;
+    let new_sym_cw_select;
+
+    function reset_ui() {
+        // Load all the existing CW information
+        let all_cws = [];
+        database.exec(`
+            select page_cw.*, count(images.id) as imgs_using
+                from page_cw left join images on images.cw_id = page_cw.id
+                group by page_cw.id`, {
+            rowMode: 'object',
+            resultRows: all_cws,
+        });
+        all_cws.sort(sorting.sort_sym_cw);
+
+        // Reset all the relevant UI
+        sym_cw_status.innerHTML = '&nbsp;';
+        sym_cw_cur_id.innerHTML = '&nbsp;';
+        sym_cw_text.value = '';
+        sym_cw_text.focus();
+        // Temporarily remove this, so that we can programmatically change the default button
+        sym_cw_change.remove();
+        sym_cw_change.disabled = true;
+        sym_cw_change.style.display = 'none';
+        sym_cw_delete.style.display = 'none';
+
+        // Make entirely new list of CWs
+        new_sym_cw_select = document.createElement('select');
+        new_sym_cw_select.id = 'sym_cw_select'
+        // Add an empty option
+        new_sym_cw_select.appendChild(document.createElement('option'));
+
+        all_sym_cw_map = new Map();
+        for (let cw of all_cws) {
+            let option = document.createElement('option');
+            option.value = cw.id;
+            option.innerText = cw.text;
+            new_sym_cw_select.appendChild(option);
+
+            all_sym_cw_map.set(cw.id, cw);
+        }
+
+        new_sym_cw_select.addEventListener('change', () => {
+            let selected_cw_id = new_sym_cw_select.value;
+            if (selected_cw_id) {
+                selected_cw_id = BigInt(selected_cw_id);
+                let cw = all_sym_cw_map.get(selected_cw_id);
+
+                sym_cw_cur_id.innerText = `Selected CW ID: ${selected_cw_id} (used by ${cw.imgs_using} symbols)`;
+                sym_cw_text.value = cw.text;
+
+                sym_cw_new.parentNode.insertBefore(sym_cw_change, sym_cw_new);
+                sym_cw_change.disabled = false;
+                sym_cw_change.style.display = '';
+                sym_cw_delete.style.display = '';
+                // Only allow delete button if there are no images using it
+                sym_cw_delete.disabled = cw.imgs_using > 0;
+            } else {
+                sym_cw_cur_id.innerHTML = '&nbsp;';
+                sym_cw_text.value = '';
+
+                sym_cw_change.remove();
+                sym_cw_change.disabled = true;
+                sym_cw_change.style.display = 'none';
+                sym_cw_delete.style.display = 'none';
+            }
+        });
+
+        let old_sym_cw_select = document.getElementById('sym_cw_select');
+        old_sym_cw_select.parentNode.replaceChild(new_sym_cw_select, old_sym_cw_select);
+    }
+
+    // The buttons to actually do things
+    function perform_input_validation(read_id) {
+        let new_text = sym_cw_text.value;
+
+        if (!new_text) {
+            sym_cw_text.focus();
+            sym_cw_status.className = "status_error";
+            sym_cw_status.innerText = "Must have some text";
+            return;
+        }
+
+        let query_data = [new_text];
+        if (read_id) {
+            query_data.push(BigInt(new_sym_cw_select.value));
+        }
+        return {
+            query_insert_args: "(text, id)",
+            query_update_args: "text = ?",
+            query_data,
+        };
+    }
+
+    sym_cw_change.onclick = () => {
+        let changed_cw = perform_input_validation(true);
+        if (changed_cw === undefined) return;
+
+        let { query_update_args, query_data } = changed_cw;
+        database.exec(`update page_cw set ${query_update_args} where id = ?`, {
+            bind: query_data
+        });
+
+        // Ok
+        reset_ui();
+
+        sym_cw_status.className = "status_ok";
+        sym_cw_status.innerText = "OK!";
+        download_changes_elem.style.visibility = '';
+    };
+    sym_cw_new.onclick = () => {
+        let new_cw = perform_input_validation(false);
+        if (new_cw === undefined) return;
+
+        let { query_insert_args, query_data } = new_cw;
+
+        let new_id = Snowflake.generate();
+        query_data.push(new_id);
+
+        let query_values = []
+        for (let i = 0; i < query_data.length; i++)
+            query_values.push('?');
+        query_values = query_values.join(', ');
+
+        database.exec(`insert into page_cw ${query_insert_args} values (${query_values})`, {
+            bind: query_data
+        });
+
+        // Ok
+        reset_ui();
+
+        sym_cw_status.className = "status_ok";
+        sym_cw_status.innerText = `OK, new id ${new_id}!`;
+        download_changes_elem.style.visibility = '';
+    };
+    sym_cw_delete.onclick = () => {
+        let sym_id = new_sym_cw_select.value;
+        if (!sym_id) {
+            sym_cw_status.className = "status_error";
+            sym_cw_status.innerText = "No CW selected";
+            return;
+        }
+        sym_id = BigInt(sym_id);
+
+        let sym_data = all_sym_cw_map.get(sym_id);
+        if (sym_data.imgs_using > 0) {
+            sym_cw_status.className = "status_error";
+            sym_cw_status.innerText = "Cannot delete, there are symbols using it";
+            return;
+        }
+
+        database.exec(`delete from page_cw where id = ?`, {
+            bind: [sym_id]
+        });
+
+        // Ok
+        reset_ui();
+
+        sym_cw_status.className = "status_ok";
+        sym_cw_status.innerText = "OK!";
+        download_changes_elem.style.visibility = '';
+    }
+
+    // Set up the UI the first time
+    reset_ui();
+}
