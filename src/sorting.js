b0aadfe082d2221b1924a49302a56c2b04b6a747
R <r@arcanenibble.com>
2026-06-11 17:57:58 +0100

    Add ability to manipulate CWs attached to individual symbols

--- src/sorting.js
+++ src/sorting.js
@@ -1,12 +1,19 @@
 export function sort_artists(a, b) {
-    a = a.display.toUpperCase();
-    b = b.display.toUpperCase();
+    a = String(a.display).toUpperCase();
+    b = String(b.display).toUpperCase();
 
     // Deal with removing "The "
     if (a.startsWith("THE "))
         a = a.slice(4);
     if (b.startsWith("THE "))
         b = b.slice(4);
+
+    return a.localeCompare(b);
+}
+
+export function sort_sym_cw(a, b) {
+    a = String(a.text).toUpperCase();
+    b = String(b.text).toUpperCase();
 
     return a.localeCompare(b);
 }
