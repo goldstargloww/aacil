063ef4f43e10891da1cf134e4573636774d83b2c
R <r@arcanenibble.com>
2026-08-05 17:02:02 +0100

    Reimplement new JS for multi-choice theme switcher

--- src/frontend.js
+++ src/frontend.js
@@ -1,49 +1,36 @@
-function should_use_dark_theme(watcher) {
-    let user_choice = localStorage.getItem("theme");
+// We are using -v2 *just in case* anybody has settings left over
+// from the previous attempt to deploy a theme switcher.
+// The suffix makes sure everyone is starting from scratch.
+const LOCALSTORAGE_THEME = "theme-v2";
+
+function load_theme() {
+    // Look up the existing choice
+    let user_choice = localStorage.getItem(LOCALSTORAGE_THEME);
     if (user_choice !== null) {
-        // There is an explicit choice here
-        return user_choice === "dark";
-    }
-
-    // Otherwise, use the system default
-    return watcher.matches;
-}
-
-function set_theme(watcher) {
-    if (should_use_dark_theme(watcher)) {
-        document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-        document.documentElement.removeAttribute("data-theme");
+        // and set the page theme, if it exists
+        document.documentElement.setAttribute("data-theme", user_choice);
     }
 }
+// On _every_ page, load the user's preferred theme
+load_theme();
 
-let system_theme_watcher = window.matchMedia("(prefers-color-scheme: dark)");
-// Set the theme once on load
-set_theme(system_theme_watcher);
-// Update the theme if the system default changes
-system_theme_watcher.addEventListener("change", set_theme);
-
+// This function _only_ runs on the "index.html" page
 function on_main_page_load() {
-    let light_theme_button = document.getElementById("light_theme");
-    let dark_theme_button = document.getElementById("dark_theme");
+    let theme_dropdown = document.getElementById("theme_switcher");
 
-    // Mark the user's choice, if there is one
-    let user_choice = localStorage.getItem("theme");
+    let user_choice = localStorage.getItem(LOCALSTORAGE_THEME);
     if (user_choice !== null) {
-        if (user_choice === "dark")
-            dark_theme_button.checked = true;
-        else
-            light_theme_button.checked = true;
+        // Make the dropdown stay in sync with the saved value
+        theme_dropdown.value = user_choice;
     }
 
-    // Handle the theme being set manually
-    light_theme_button.addEventListener("click", () => {
-        localStorage.setItem("theme", "light");
-        set_theme(system_theme_watcher);
-    });
-    dark_theme_button.addEventListener("click", () => {
-        localStorage.setItem("theme", "dark");
-        set_theme(system_theme_watcher);
+    // Handle updates
+    theme_dropdown.addEventListener("change", () => {
+        let theme = theme_dropdown.value;
+        // Set the page theme
+        document.documentElement.setAttribute("data-theme", theme);
+        // *and* remember it for later
+        localStorage.setItem(LOCALSTORAGE_THEME, theme);
     });
 }
 // Make sure function is exported to HTML
